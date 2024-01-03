8e96e016f0cfb4d28033ae2ae0a862c773a28308
Maxence Lavenu <maxence.lavenu@outlook.fr>
2024-01-03 23:44:50 +0100

    Refactor URLs in main.js and update metadata.json

--- main.js
+++ main.js
@@ -90,7 +90,8 @@ window.addEventListener('DOMContentLoaded', () => {
             if (item.url) {
                 let itemUrl = document.createElement("a");
                 itemUrl.classList.add("url");
-                itemUrl.innerText = item.url;
+                // for the inner text, use the url without the protocol and trailing slashes
+                itemUrl.innerText = item.url.replace(/(^\w+:|^)\/\//, '').replace(/\/$/, "");
                 itemUrl.href = item.url;
                 itemUrl.target = "_blank";
                 itemContainer.appendChild(itemUrl);
